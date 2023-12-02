e4ca8f53482c5d41c1bd333e69e3d637b1578c88
ekougs <ekougs@gmail.com>
2023-12-02 13:32:04 +0100

    [2023][day1] Trebuchet (2/2)

--- src/main.rs
+++ src/main.rs
@@ -24,42 +24,45 @@ mod trebuchet;
 // Dir size < 100_000 1477771
 // Dir size < 100_000 3579501
 // Nb of visible trees 1684
+// Trebuchet digits 56506
+// Trebuchet digits and letters 56017
 
 fn main() {
     // 2022
     // Day 1
-//    println!("Calories {}", calories::count_calories("calories_input.txt", 1));
-//    println!("Calories {}", calories::count_calories("calories_input.txt", 3));
+    println!("Calories {}", calories::count_calories("calories_input.txt", 1));
+    println!("Calories {}", calories::count_calories("calories_input.txt", 3));
 
     // Day 2
-//    println!("Rock paper scissor {}", rps::score_as_provided("rock_paper_scissor_input.txt"));
-//    println!("Rock paper scissor {}", rps::score_as_ordered("rock_paper_scissor_input.txt"));
+    println!("Rock paper scissor {}", rps::score_as_provided("rock_paper_scissor_input.txt"));
+    println!("Rock paper scissor {}", rps::score_as_ordered("rock_paper_scissor_input.txt"));
 
     // Day 3
-//    println!("Rucksacks' priorities {}", rucksack::prioritize_for_diff_in_rumsack("rucksacks_input.txt"));
-//    println!("Rucksacks' priorities {}", rucksack::prioritize_for_group_common("rucksacks_input.txt"));
+    println!("Rucksacks' priorities {}", rucksack::prioritize_for_diff_in_rumsack("rucksacks_input.txt"));
+    println!("Rucksacks' priorities {}", rucksack::prioritize_for_group_common("rucksacks_input.txt"));
 
     // Day 4
-//    println!("Covering pairs {}", pairs::covering("covering_pairs_input.txt"));
-//    println!("Covering pairs {}", pairs::overlaping("covering_pairs_input.txt"));
+    println!("Covering pairs {}", pairs::covering("covering_pairs_input.txt"));
+    println!("Covering pairs {}", pairs::overlaping("covering_pairs_input.txt"));
 
     // Day 5
-//    println!("Arrange crates {}", crates::arrange_one_by_one("crates_input.txt"));
-//    println!("Arrange crates {}", crates::arrange_mult("crates_input.txt"));
+    println!("Arrange crates {}", crates::arrange_one_by_one("crates_input.txt"));
+    println!("Arrange crates {}", crates::arrange_mult("crates_input.txt"));
 
     // Day 6
-//    println!("Marker starts at {}", packet::start_idx("start_of_packet_marker_input.txt", 4));
-//    println!("Message starts at {}", packet::start_idx("start_of_packet_marker_input.txt", 14));
+    println!("Marker starts at {}", packet::start_idx("start_of_packet_marker_input.txt", 4));
+    println!("Message starts at {}", packet::start_idx("start_of_packet_marker_input.txt", 14));
 
     // Day 7
-//    println!("Dir size < 100_000 {}", dirs::dirs_size("candidate_directories_input.txt", 100_000));
-//    println!("Dir size < 100_000 {}", dirs::min_dir_size_to_free("candidate_directories_input.txt", 70_000_000, 30_000_000));
+    println!("Dir size < 100_000 {}", dirs::dirs_size("candidate_directories_input.txt", 100_000));
+    println!("Dir size < 100_000 {}", dirs::min_dir_size_to_free("candidate_directories_input.txt", 70_000_000, 30_000_000));
 
     // Day 8
-//    println!("Nb of visible trees {}", trees::nb_visible_trees("tree_house_input.txt"));
+    println!("Nb of visible trees {}", trees::nb_visible_trees("tree_house_input.txt"));
 
     // 2023
     // Day 1
-    println!("Trebuchet {}", trebuchet::calibrate("trebuchet_input.txt"));
+    println!("Trebuchet {}", trebuchet::calibrate_using_digits("trebuchet_input.txt"));
+    println!("Trebuchet with letters {}", trebuchet::calibrate_using_letters_and_digits("trebuchet_input.txt"));
 
 }
--- src/trebuchet.rs
+++ src/trebuchet.rs
@@ -1,45 +1,55 @@
-use std::path::Path;
-use std::fs::File;
-use std::io::prelude::*;
+use std::collections::HashMap;
+use regex::Regex;
 use crate::utils::lines;
+use lazy_static::lazy_static;
 
-pub fn calibrate(trebuchet_filename: &str) -> u32 {
-    let trebuchet_input_path = Path::new(trebuchet_filename);
-    let display = trebuchet_input_path.display();
-    
-    // Open the path in read-only mode, returns `io::Result<File>`
-    let mut file = match File::open(&trebuchet_input_path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
-        Ok(file) => file,
-    };
-    
-    let mut calories_file_content = String::new();
-    match file.read_to_string(&mut calories_file_content) {
-        Err(why) => panic!("couldn't read {}: {}", display, why),
-        Ok(_) => {}
-    };
-
-    let mut total_calibration = 0;
-    if let Ok(lines) = lines(trebuchet_filename) {
-        for maybe_line in lines {
-            if let Ok(line) = maybe_line {
-                total_calibration += to_calibration(line)
-            }
-        }
-    }
-    total_calibration
+lazy_static! {
+    static ref DIGITS_OR_LETTERS_REGEX: Regex = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine|\d)").unwrap();
+    static ref LETTERS_TO_DIGIT: HashMap<&'static str, u32> = HashMap::from(
+        [("one", 1), ("two", 2),("three", 3),("four", 4),("five", 5),("six", 6),("seven", 7),("eight", 8),("nine", 9)],
+    );
 }
 
-fn to_calibration(line: String) -> u32 {
+pub fn calibrate_using_letters_and_digits(trebuchet_filename: &str) -> u32 {
+    match lines(trebuchet_filename) {
+        Err(why) => {
+            panic!("Couldn't open {}: {}", trebuchet_filename, why)
+        },
+        Ok(mut lines) => {
+            let mut total_calibration = 0;
+            while let Some(Ok(line)) = lines.next() {
+                total_calibration += to_calibration_letters_and_digits(&line);
+            }
+            total_calibration
+        }
+    }
+}
+
+pub fn calibrate_using_digits(trebuchet_filename: &str) -> u32 {
+    match lines(trebuchet_filename) {
+        Err(why) => {
+            panic!("Couldn't open {}: {}", trebuchet_filename, why)
+        },
+        Ok(mut lines) => {
+            let mut total_calibration = 0;
+            while let Some(Ok(line)) = lines.next() {
+                total_calibration += to_calibration_digits(line);
+            }
+            total_calibration
+        }
+    }
+}
+
+fn to_calibration_digits(line: String) -> u32 {
     let mut first_digit: Option<u32> = None;
     let mut last_digit: Option<u32> = None;
     let mut line_chars = line.chars().peekable();
-    while line_chars.peek().is_some() {
-        match line_chars.next() {
+    while let Some(char) = line_chars.next() {
+        match char {
             // The first calibration digit
-            Some(line_char) if line_char.is_ascii_digit() && first_digit.is_none() => first_digit = Some(line_char.to_digit(10).unwrap()),
+            char if char.is_ascii_digit() && first_digit.is_none() => first_digit = Some(char.to_digit(10).unwrap()),
             // The second calibration digit
-            Some(line_char) if line_char.is_ascii_digit() && first_digit.is_some() => last_digit = Some(line_char.to_digit(10).unwrap()),
+            char if char.is_ascii_digit() && first_digit.is_some() => last_digit = Some(char.to_digit(10).unwrap()),
             _ => continue,
         }
     }
@@ -49,5 +59,37 @@ fn to_calibration(line: String) -> u32 {
     if last_digit.is_none() {
         last_digit = first_digit;
     }
+    first_digit.unwrap() * 10 + last_digit.unwrap()
+}
+
+fn to_calibration_letters_and_digits(line: &str) -> u32 {
+    let mut first_digit: Option<u32> = None;
+    let mut last_digit: Option<u32> = None;
+    let mut locs = DIGITS_OR_LETTERS_REGEX.capture_locations();
+    let mut start = 0;
+    let mut opt_captures = DIGITS_OR_LETTERS_REGEX.captures_read_at(&mut locs, line, start);
+
+    while let Some(captures) = opt_captures {
+        let digit_or_letters = captures.as_str();
+        let digit: u32 = match digit_or_letters {
+             letters if LETTERS_TO_DIGIT.contains_key(&letters) => *LETTERS_TO_DIGIT.get(&letters).unwrap(),
+             _ => digit_or_letters.chars().next().unwrap().to_digit(10).unwrap()
+        };
+        if first_digit.is_none() {
+            first_digit = Some(digit);
+        } else {
+            last_digit = Some(digit);
+        }
+         start = locs.get(1).unwrap().0 + 1;
+         opt_captures = DIGITS_OR_LETTERS_REGEX.captures_read_at(&mut locs, line, start);
+    }
+    if first_digit.is_none() {
+        first_digit = Some(0);
+    }
+    if last_digit.is_none() {
+        last_digit = first_digit;
+    }
+
+    // println!("Calibration {}", first_digit.unwrap() * 10 + last_digit.unwrap());
     first_digit.unwrap() * 10 + last_digit.unwrap()
 }
